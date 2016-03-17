797e8041d003c61a14b89242df8b1a7f33edab21
Robert Starmer <rstarmer@gmail.com>
2016-03-17 02:32:29 -0700

    Update cluster config for disk and ycsb

--- packet/get_kubectl.sh
+++ packet/get_kubectl.sh
@@ -1,4 +1,4 @@
-export K8S_VERSION=1.2.0-alpha.7
+export K8S_VERSION=1.2.0-beta.1
 wget http://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl
 chmod 755 kubectl
 mv kubectl /usr/local/bin/
